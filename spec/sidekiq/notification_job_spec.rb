3999f9c11b2de4c447c7b2ae991f5105b605c4d8
Ho Hoang Lam <hoanglamuet@gmail.com>
2024-03-18 03:16:08 +0700

    Add more expectations

--- spec/sidekiq/notification_job_spec.rb
+++ spec/sidekiq/notification_job_spec.rb
@@ -12,20 +12,24 @@ RSpec.describe NotificationJob, type: :job do
     end
 
     context 'when video id does not exist' do
+      let(:video) { build(:video, id: 1) }
+
       it 'does not broadcast the notification' do
-        allow(Video).to receive(:find_by_id).and_return(nil)
+        allow(Video).to receive(:find_by_id).with(video.id)
         job
         described_class.drain
+        expect(Video).to have_received(:find_by_id).with(video.id)
         expect(ActionCable.server).not_to receive(:broadcast)
       end
     end
 
     context 'when video id exists' do
       it 'broadcast the notification' do
-        allow(Video).to receive(:find_by_id).and_return(video)
+        allow(Video).to receive(:find_by_id).with(video.id).and_return(video)
         allow(ActionCable.server).to receive(:broadcast)
         job
         described_class.drain
+        expect(Video).to have_received(:find_by_id).with(video.id)
         serialized_video = VideoSerializer.new(video, { include: [:user], fields: { video: [:title] } })
                                           .serializable_hash
         expect(ActionCable.server).to have_received(:broadcast).with('notifications_channel', serialized_video)
